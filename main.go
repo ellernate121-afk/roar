f8ed4ba4c3efd63026705a8aa48087a2e121e6b8
Shanivator <ellernate121@gmail.com>
2026-05-25 11:56:08 -0400

    Move main.go to root for Render compatibility

---
+++ main.go
@@ -0,0 +1,28 @@
+// The roar command.
+package main
+
+import (
+	"os"
+
+	"github.com/anaminus/snek"
+	"github.com/robloxapi/roar/generate"
+)
+
+var Program = snek.NewProgram("roar", os.Args)
+
+func init() {
+	Program.Register(generate.Def)
+}
+
+func main() {
+	Program.Usage(usage)
+	Program.Main()
+}
+
+const usage = `%s [command]
+
+Handles generation of Roblox Luau API data for a Hugo website.
+
+The following commands are available:
+%s
+`
